Corrected the notation for the vec_h (notR calculation) case of normal peers

computation.py
@@ -1,13 +1,13 @@
 ## @package computation
 # contains functions related to the theoritical proof of convergence equation
-# # R ∞ = (I − A[normal] )^−1 ( h + A[Forceful] ∗ R[F])
+# R = (I - A[normal])^-1 ( h + A[forceful] * R[F])
 from scipy import linalg
 import numpy as np
 import networkx as nx
 
 ##
 # creates the A matrix that holds the (1-alpha)/deg part of the opinion calculation equation
-# R ∞ = (I − A[normal] )^−1 ( h + A[Forceful] ∗ R[F])
+# R = (I - A[normal])^-1 ( h + A[forceful] * R[F])
 # returns a square matrix with zeros in the diagonal and (1-alpha)/deg at indices representing neighbors
 # @param A graph from which the A matrix will be extracted
 # @param alpha weigh given to self opinion
@@ -28,15 +28,16 @@ def mat_A(G, alpha):
 	return A;
 
 ##
-# creates the R vector that contains the opinions of all nodes in the graph
-# R ∞ = (I − A[normal] )^−1 ( h + A[Forecful] ∗ R[F])
+# creates the h vector that contains the opinions of all nodes in the graph multiplied by alpha
+# R = (I - A[normal])^-1 ( h + A[forceful] * R[F])
 # @param Graph of nodes to retrieve the opinions from
 #
-def vec_R(G):
+def vec_h(G, alpha):
 	n = nx.number_of_nodes(G)
 	# initialize vector R to zeros
-	R = np.zeros(shape=(n,1))
+	h = np.zeros(shape=(n,1))
 	#iterate all nodes in the graph
 	for i in range(0,n):
-		R[i] = G.node[i]['opinion'] 
-	return R;
+		h[i] = G.node[i]['opinion']
+	h = h*alpha
+	return h;

main.py
@@ -24,12 +24,12 @@ gm.assign_normal(G)
 A = np.zeros(shape=(NUM_PEERS,NUM_PEERS))
 A = cp.mat_A(G,ALPHA)
 
-# Calculate vector R
-R = np.zeros(shape=(NUM_PEERS,1))
-R = cp.vec_R(G)
+# Calculate vector h
+h = np.zeros(shape=(NUM_PEERS,1))
+h = cp.vec_h(G, ALPHA)
 
 print A
-print R
+print h
 
 # maximum difference variable to indicate the change in opinion after local update
 max_diff = 1